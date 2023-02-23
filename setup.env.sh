f8ab1bc2042ebe5b5f8500ed78078fa08cde6e10
R&D Azure Script <rndazurescript@users.noreply.github.com>
2023-02-23 22:34:41 +0200

    Allow script to rerun

--- setup.env.sh
+++ setup.env.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 ## YOU CAN RUN THIS SCRIPT FROM THE AZURE CLI OR FROM THE AZURE CLOUD SHELL
@@ -12,7 +11,7 @@ export HUB=iot-$SUFIX-hub
 export ACR=iotacr$SUFIX
 export VNET_WE=iot-$SUFIX-vnet-we
 export VNET_NE=iot-$SUFIX-vnet-ne
-export VM_NAME='edgevm-'$SUFIX
+export VM_DNS_PREFIX='edgevm-'$SUFIX
 
 echo "Preparing testing environment for BC/DR demo."
 echo "SUFIX=$SUFIX"
@@ -27,19 +26,29 @@ echo "Creating resource group $RSG"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ## CREATE RESOURCE GRUOP
 az group create --name $RSG --location westeurope
-az group create --name $RSG_NE --location westeurope
+az group create --name $RSG_NE --location northeurope
 
 echo "Creating IoT Hub $HUB"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ## PROVISION IOT HUB
-az iot hub create --resource-group $RSG --name $HUB --sku S1 --partition-count 4
-
+if [ -n "$(az iot hub list --resource-group $RSG --query "[?name=='$HUB'].id" -o tsv)" ]; then
+   echo "$HUB exists"
+else
+   az iot hub create --resource-group $RSG --name $HUB --sku S1 --partition-count 4
+fi
 echo "Creating IoT Edge Device 'edge101' and IoT Device 'thermostat1'"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ## REGISTER DEVICES
-az iot hub device-identity create --device-id edge101 --edge-enabled --hub-name $HUB --resource-group $RSG
-az iot hub device-identity create --device-id thermostat1 --hub-name $HUB --resource-group $RSG
-
+if [ -n "$(az iot hub device-identity list --hub-name $HUB --resource-group $RSG --query "[?deviceId=='edge101'].deviceId" -o tsv)" ]; then
+   echo "edge101 exists"
+else
+   az iot hub device-identity create --device-id edge101 --edge-enabled --hub-name $HUB --resource-group $RSG
+fi
+if [ -n "$(az iot hub device-identity list --hub-name $HUB --resource-group $RSG --query "[?deviceId=='thermostat1'].deviceId" -o tsv)" ]; then
+   echo "thermostat1 exists"
+else
+  az iot hub device-identity create --device-id thermostat1 --hub-name $HUB --resource-group $RSG
+fi
 ## SHOW DEVICES CONN STRINGS
 az iot hub device-identity connection-string show --device-id edge101 --hub-name $HUB --resource-group $RSG
 az iot hub device-identity connection-string show --device-id thermostat1 --hub-name $HUB --resource-group $RSG
@@ -50,16 +59,31 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 az deployment group create \
 --resource-group $RSG \
 --template-uri "https://raw.githubusercontent.com/Azure/iotedge-vm-deploy/1.4/edgeDeploy.json" \
---parameters dnsLabelPrefix=$VM_NAME \
+--parameters dnsLabelPrefix=$VM_DNS_PREFIX \
 --parameters adminUsername='azureUser' \
 --parameters deviceConnectionString=$(az iot hub device-identity connection-string show --device-id edge101 --hub-name $HUB --resource-group $RSG -o tsv) \
 --parameters authenticationType='password' \
 --parameters adminPasswordOrKey="vmPass#word"
 
+echo "Wait for VM to boot"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+VM_NAME=$(az vm list -g $RSG -d --query "[?starts_with(fqdns,'$VM_DNS_PREFIX')].name" -o tsv)
+while true
+do
+    powerState=$(az vm show --resource-group $RSG --name $VM_NAME -d --query "powerState" -o tsv)
+    if [ "$powerState" != "VM running" ]
+    then
+        echo "VM is booting. Sleeping for a bit..."
+        sleep 10
+    else
+        echo "VM is running. Continuing with script..."
+        break
+    fi
+done
+
 echo "Configuring VM SSH on port 2223"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ## CONFIGURE VM SSH ON PORT 2223
-VM_NAME=$(az vm list -g $RSG --query "[].name" -o tsv)
 az vm run-command invoke -g $RSG  -n $VM_NAME --command-id RunShellScript --scripts "sudo sed -i -e 's/#Port 22/Port 2223/' /etc/ssh/sshd_config"
 az vm run-command invoke -g $RSG  -n $VM_NAME --command-id RunShellScript --scripts "sudo cat /etc/ssh/sshd_config"
 az vm run-command invoke -g $RSG  -n $VM_NAME --command-id RunShellScript --scripts "sudo systemctl restart ssh"
@@ -74,15 +98,16 @@ az network nsg rule create --resource-group $RSG --nsg-name $NSG_NAME --name Blo
 echo "Configuring VNETs"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ## SETUP VNETS
-VNET_VM=$(az network vnet list -g $RSG --query "[].name" -o tsv)
+VNET_VM=$(az network vnet list -g $RSG --query "[?starts_with(name,'vnet-')].name" -o tsv)
 az network vnet create --resource-group $RSG --name $VNET_WE --address-prefix 10.1.0.0/16 --subnet-name default --subnet-prefix 10.1.0.0/24 --location westeurope
 az network vnet create --resource-group $RSG_NE --name $VNET_NE --address-prefix 10.2.0.0/16 --subnet-name default --subnet-prefix 10.2.0.0/24 --location northeurope
  
 echo "Configuring VNET peering"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+VNET_WE_ID=$(az network vnet show -g $RSG -n $VNET_WE --query id -o tsv)
 VNET_NE_ID=$(az network vnet show -g $RSG_NE -n $VNET_NE --query id -o tsv)
-az network vnet peering create --name vnet-onprem-to-vnet-we --resource-group $RSG --vnet-name $VNET_VM --remote-vnet $VNET_WE --allow-vnet-access
-az network vnet peering create --name vnet-onprem-to-vnet-ne --resource-group $RSG --vnet-name $VNET_VM --remote-vnet $VNET_NE  --remote-vnet $VNET_NE_ID --allow-vnet-access
+az network vnet peering create --name vnet-onprem-to-vnet-we --resource-group $RSG --vnet-name $VNET_VM --remote-vnet $VNET_WE_ID --allow-vnet-access
+az network vnet peering create --name vnet-onprem-to-vnet-ne --resource-group $RSG --vnet-name $VNET_VM --remote-vnet $VNET_NE_ID --allow-vnet-access
 
 
 echo "Configuring Storage Account"
@@ -91,13 +116,13 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 az storage account create --name iotstg$SUFIX --resource-group $RSG --location westeurope --sku Standard_LRS --kind StorageV2
 
 az acr create --resource-group $RSG --name $ACR --sku Premium --admin-enabled true
-az acr import --resource-group $RSG --name $ACR --source mcr.microsoft.com/azureiotedge-simulated-temperature-sensor:latest --image azureiotedge-simulated-temperature-sensor:latest
+az acr import --resource-group $RSG --name $ACR --source mcr.microsoft.com/azureiotedge-simulated-temperature-sensor:latest --image azureiotedge-simulated-temperature-sensor:latest --force
 
 ## MANUAL ACTIONS
 # - ENABLE PRIVATE LINK FOR WE
 # Ref: https://learn.microsoft.com/en-us/azure/iot-hub/iot-hub-public-network-access
-
-echo "To connect to the VM run: ssh -p 2223 azureUser@$VM_NAME.westeurope.cloudapp.azure.com password: vmPass#word"
+VM_FQDN=$(az vm show -d --resource-group $RSG --name $VM_NAME --query "fqdns" -o tsv)
+echo "To connect to the VM run: ssh -p 2223 azureUser@$VM_FQDN password: vmPass#word"
 echo "You will need to MANUALLY create the private endpoints in the WE VNET (and NE when required) for the IoT Hub"
 echo "ALSO you need to create the PE for the ACR"
 echo "You will need to configure the IoT Edge device (edge101) to deploy the TemperatureSensorModule from the market place"
